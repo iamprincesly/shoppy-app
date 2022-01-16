6e2b5138a120535d4e5188a24989e1cdf035ae8f
iamprincesly <iamprincesly@gmail.com>
2022-01-16 18:31:04 +0100

    Break homepage to smaller components and styled menu items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
-    return <div>Hello word</div>;
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
